d9dbf1cfd47756593f5e98939e112e19bbb4c724
DarkySparkyx <162906720+DarkySparkyx@users.noreply.github.com>
2024-03-22 11:43:09 +0100

    project cd

---
+++ 2 SEMESTER/Object Oriented Programming/Projects/CPP_PROJ1/Swiat.cpp
@@ -0,0 +1,4 @@
+#include "Swiat.h"
+
+
+
---
+++ 2 SEMESTER/Object Oriented Programming/Projects/CPP_PROJ1/Swiat.h
@@ -0,0 +1,17 @@
+#ifndef SWIAT_H
+#define SWIAT_H
+
+class Swiat {
+private:
+    //WYMIARY SWIATA
+    int x;
+    int y;
+public:
+    Swiat();
+    void wykonajTure();
+    void rysujSwiat();
+
+};
+
+
+#endif //SWIAT_H
